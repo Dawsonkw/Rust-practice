8e4ba7776f5992ddb170d1b94be763caca5db37d
Dawsonkw <dawsonkw@gmail.com>
2025-01-13 11:46:37 -0700

    finished chapter, built project to change farenheit to celsius and kelvin, 2 other projects for later when Im bored

--- src/main.rs
+++ src/main.rs
@@ -110,3 +110,175 @@
     // fn print_labeled_measurement(value: i32, unit_label: char) {
     //     println!("the measurement is: {value}{unit_label}");
     // }
+
+
+//  fn main() {
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+
+//     println!("The value of y is: {}", y);
+//  }
+
+// Functions with return values
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let x = plus_one(5);
+
+//     println!("The value of x is: {}", x);
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1
+// }
+
+// Control flow
+
+// fn main() {
+//     let number = 3;
+
+//     if number <5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+    // fn main() {
+    //     let number: i64 = 566456;
+
+    //     if number % 4 == 0 {
+    //         println!("number is divisible by 4");
+    //     } else if number % 3 == 0 {
+    //         println!("number is divisible by 3");
+    //     } else if number % 2 == 0 {
+    //         println!("number is divisible by 2");
+    //     } else {
+    //         println!("number is not divisible by 4, 3, or 2");
+    //     }
+    // }
+
+    // fn main() {
+    //     let condition = true;
+
+    //     let number = if condition { 5 } else { 6 };
+
+    //     println!("The value of number is: {}", number);
+    // }
+
+    // fn main() {
+    //     loop {
+    //         println!("again!");
+    //     }
+    // }
+
+    // fn main() {
+    //     let mut counter = 0;
+
+    //     let result = loop {
+    //         counter += 1;
+
+    //         if counter == 42069 {
+    //             break counter * 2;
+    //         }
+    //     };
+
+    //     println!("The result is {}", result);
+    // }
+
+    // fn main() {
+    //     let mut count = 0;
+
+    //     'counting_up: loop{
+    //         println!("count is: {count}");
+    //         let mut remaining = 10;
+
+    //         loop {
+    //             println!("remaining is: {remaining}");
+    //             if remaining == 9 {
+    //                 break;
+    //             }
+    //             if count == 2 {
+    //                 break 'counting_up;
+    //             }
+    //             remaining -= 1;
+    //         }
+
+    //         count += 1;
+    //     }
+
+    //     println!("End count is: {count}");
+    // }
+
+    // fn main() {
+    //     let mut number = 3;
+
+    //     while number != 0 {
+    //         println!("{}", number);
+
+    //         number -= 1;
+    //     }
+
+    //     println!("LIFTOFF!!");
+    // }
+
+    // fn main() {
+    //     let a = [10, 20, 30, 40, 50];
+    //     let mut index = 0 ;
+
+    //     while index < 5 {
+    //         println!("the value is: {}", a[index]);
+    //         index += 1;
+    //     }
+    // }
+
+    // fn main() {
+    //     let a = [10, 20, 30, 40, 50];
+
+    //     for element in a {
+    //         println!("the value is: {}", element);
+    //     }
+    // }
+
+    // fn main() {
+    //     for number in (1..4).rev() {
+    //         println!("{}!", number);
+    //     }
+    //     println!("LIFTOFF!!");
+    // }
+
+
+// Chapter projects 
+
+
+
+    //Convert temperatures between Fahrenheit and Celsius.
+    fn main() {
+        // Get the temperature input from the user
+        println!("Enter a temperature in Fahrenheit:");
+        let mut fahrenheit = String::new();
+        std::io::stdin().read_line(&mut fahrenheit).expect("Failed to read line");
+        let fahrenheit: f32 = fahrenheit.trim().parse().expect("Please enter a valid number");
+
+        // Convert the temperature to Celsius and Kelvin
+        let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+        let kelvin = celsius + 273.15;
+
+        // Print the result
+        println!("{} degrees Fahrenheit is equal to {} degrees Celsius, and {} degrees Kelvin", fahrenheit, celsius, kelvin);
+
+    }
+
+    // Generate the nth Fibonacci number.
+
+    // Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
