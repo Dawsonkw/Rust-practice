40abebe30639f4b0d17a81cda85b0f5d02ad23d7
Dawsonkw <dawsonkw@gmail.com>
2025-04-01 18:09:00 -0600

    blind push broke the layout. cest la vie

--- src/main.rs
+++ src/main.rs
@@ -280,5 +280,6 @@
     }
 
     // Generate the nth Fibonacci number.
+        
 
     // Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
